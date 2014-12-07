761cec1c42bf6fa567cdc331cab9a7abf7e4491c
PharaohKJ <kato@phalanxware.com>
2014-12-07 15:38:11 +0900

    table変更を覚えた。

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141207061408) do
+ActiveRecord::Schema.define(version: 20141207063120) do
 
   create_table "users", force: true do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "aaa"
   end
 
 end
