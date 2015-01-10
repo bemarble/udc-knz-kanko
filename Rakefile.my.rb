3bf21fb40b6604251318399add53fea782090dc9
PharaohKJ <kato@phalanxware.com>
2015-01-10 15:57:11 +0900

    heroku側でrakeするためにRakefile名の変更。

---
+++ Rakefile.my.rb
@@ -0,0 +1,34 @@
+# coding: utf-8
+
+task default: [:help] do
+end
+
+desc '説明書'
+task help: [] do
+  sh 'bundle exec rake -T'
+end
+
+desc 'テスト用手元実行'
+task f: [] do
+  sh 'foreman start'
+end
+
+namespace :setup do
+
+  desc 'init setup'
+  task init: [] do
+    sh 'bundle install --path vendor/bundle'
+  end
+
+  desc 'bundle install'
+  task bundle: [] do
+    sh 'bundle install --path vendor/bundle'
+  end
+
+  task print_git_remote: [] do
+    puts '以下の設定でやることをオススメ'
+    puts 'git remote set-url origin git@github.com:bemarble/udc-knz-kanko.git'
+    puts 'git remote add heroku  git@heroku.com:udc-knz-kanko.git'
+  end
+
+end
