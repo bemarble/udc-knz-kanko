9e6f062916f75c15d4cdb0224160bdd2c2a64c65
PharaohKJ <kato@phalanxware.com>
2015-01-10 19:15:13 +0900

    production時はenvを読まないように。

--- main.rb
+++ main.rb
@@ -5,9 +5,6 @@ require 'active_record'
 
 require './model.rb'
 
-ActiveRecord::Base.configurations = YAML.load_file('config/database.yml')
-ActiveRecord::Base.establish_connection('development')
-
 class User < ActiveRecord::Base
 end
 
