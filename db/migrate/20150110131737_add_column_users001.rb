4dfd9e5d59eb3eb3d8a3b0849f3887fe1f285024
BeMarble <mktkbys@gmail.com>
2015-01-17 13:58:54 +0900

    change_tableを削除

--- db/migrate/20150110131737_add_column_users001.rb
+++ db/migrate/20150110131737_add_column_users001.rb
@@ -1,8 +1,5 @@
 class AddColumnUsers001 < ActiveRecord::Migration
   def change
-    change_table :users do |t|
-      t.timestamp :updated_at
-    end
 
     add_index :users, :twitter
     add_index :users, :facebook
