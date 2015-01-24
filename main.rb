9b116ac0b06abea609cc176be5bc2c2c37ddebe2
BeMarble <mktkbys@gmail.com>
2015-01-24 16:33:03 +0900

    メッセージ登録処理

--- main.rb
+++ main.rb
@@ -11,6 +11,9 @@ end
 class Post < ActiveRecord::Base
 end
 
+class Message  < ActiveRecord::Base
+end
+
 require 'omniauth'
 require 'omniauth-facebook'
 require 'omniauth-twitter'
@@ -137,7 +140,7 @@ class App < Sinatra::Base
       :features => []
     }
 
-    list = Post.order("created_at DESC").limit(10).map do |row|
+    list = Post.find_by_sql(['SELECT * FROM posts AS p WHERE updated_at = (SELECT MAX(updated_at) FROM posts as s WHERE p.user_id=s.user_id) LIMIT 10']).map do |row|
       record = {
         :type => "Feature",
         :properties => {
@@ -162,11 +165,22 @@ class App < Sinatra::Base
 
   end
 
+  post '/user/' do
+
+    if @params[:id]
+      id = @params[:id]
+      row = User.find(id)
+    else
+      row = {}
+    end
+    row.to_json
+  end
+
   get '/login/' do
     erb :login
   end
 
-  get '/my' do
+  get '/me' do
 
     @user = session
 
@@ -180,7 +194,6 @@ class App < Sinatra::Base
   end
 
   post '/register_state' do
-    message =
 
     posts = Post.new
 
@@ -188,7 +201,8 @@ class App < Sinatra::Base
     posts.message = @params[:message]
     posts.latitude = @params[:lat]
     posts.longitude = @params[:lng]
-    posts.save
 
+
+    posts.save
   end
 end
